01fee8d2ccb5f145ac75a9240599bd9cdbcf71e8
Zach Jones <zmjones14@gmail.com>
2017-06-27 12:42:17 -0500

    Section 2 Lecture 38 - Section complete
    Covered basics of string interpolation, property binding, event binding,
    two-way databinding. Also learned about standard directives, e.g.
    ngStyle, ngClass, ngIf, ngFor.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,16 +4,39 @@ import { Component, OnInit } from '@angular/core';
   //selector: '[app-servers]'
   //selector: '.app-servers'
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['Tsetserver', 'Testserver 2'];
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit() {
+  }
+
+  onCreateServer() 
+  {
+    this.serverCreated = true;
+    this.servers.push(this.serverName)
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) 
+  {
+    //console.log(event);
+    this.serverName = event.target.value;
   }
 
 }
